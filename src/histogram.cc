1295b78de79a2a760c5d6fb86293fafd5a9b721d
Sam Hasinoff <hasinoff@google.com>
2026-09-11 17:00:47 -0700

    Optimize ARM NEON histogramming and Trellis dynamic programming search

--- src/histogram.cc
+++ src/histogram.cc
@@ -80,18 +80,20 @@ void StoreHistoSSE2(const int16_t in[64], Histo* const histos, int nb_blocks) {
 void StoreHistoNEON(const int16_t in[64], Histo* const histos, int nb_blocks) {
   const uint16x8_t kMaxHisto = vdupq_n_u16(MAX_HISTO_DCT_COEFF);
   for (int n = 0; n < nb_blocks; ++n, in += 64) {
-    uint16_t tmp[64];
     for (int i = 0; i < 64; i += 8) {
       const int16x8_t A = vld1q_s16(in + i);
       const int16x8_t B = vabsq_s16(A);               // abs(in)
       const uint16x8_t C = vreinterpretq_u16_s16(B);  // signed->unsigned
       const uint16x8_t D = vshrq_n_u16(C, HSHIFT);    // >>= HSHIFT
       const uint16x8_t E = vminq_u16(D, kMaxHisto);   // min(.,kMaxHisto)
-      vst1q_u16(tmp + i, E);
-    }
-    for (int j = 0; j < 64; ++j) {
-      const int k = tmp[j];
-      ++histos->counts_[j][k];
+      ++histos->counts_[i + 0][vgetq_lane_u16(E, 0)];
+      ++histos->counts_[i + 1][vgetq_lane_u16(E, 1)];
+      ++histos->counts_[i + 2][vgetq_lane_u16(E, 2)];
+      ++histos->counts_[i + 3][vgetq_lane_u16(E, 3)];
+      ++histos->counts_[i + 4][vgetq_lane_u16(E, 4)];
+      ++histos->counts_[i + 5][vgetq_lane_u16(E, 5)];
+      ++histos->counts_[i + 6][vgetq_lane_u16(E, 6)];
+      ++histos->counts_[i + 7][vgetq_lane_u16(E, 7)];
     }
   }
 }
